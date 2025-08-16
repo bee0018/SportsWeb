b53ef23019db052d35a6e296287adf8145568d60
Bryce Ellis <91974239+bee0018@users.noreply.github.com>
2025-08-16 12:55:34 -0500

    Added initial boilerplate code to talk to the backend services

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 - For version control download SourceTree for preferred OS: https://www.sourcetreeapp.com/
 - Download Node JS from https://nodejs.org/en/download/.
 - Download WebStorm from https://www.jetbrains.com/webstorm/promo/?source=google&medium=cpc&campaign=AMER_en_US-CST_WebStorm_Branded&term=webstorm&content=717267885243&gad_source=1&gad_campaignid=9641686287&gbraid=0AAAAADloJzjM8YwGuomM1PAAElS0TYUtX&gclid=Cj0KCQjw2IDFBhDCARIsABDKOJ6ZAX4ejAlVLQhNXvOmGcZ6rUg8tSvULOMdTD4DFZLUPnMA5E5bJkIaAmStEALw_wcB. Suggested that you use the non-commercial version for the time being.
+
+## Running the Front End
+- In your terminal, `cd sports-web-app` and enter `npm run start`. (For bryce use the command prompt in windows)
---
+++ sports-web-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+async function fetchTodo() {
+    const result = await fetch("https://localhost:44369/todo?id=4&scream=true")
+    const data = await result.json();
+    console.log(data);
+}
+
+function App() {
+    fetchTodo();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
