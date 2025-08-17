db0800f57bb3ae3b546067a46472ae16ed867da7
Bryce Ellis <91974239+bee0018@users.noreply.github.com>
2025-08-17 16:39:03 -0500

    Added documentation for the starter code

--- SportsWebAPI/Requests/TodoRequest.cs
+++ SportsWebAPI/Requests/TodoRequest.cs
@@ -1,8 +1,18 @@
 ﻿namespace SportsWebAPI.Requests
 {
+    /// <summary>
+    /// Class representing the basis for a todo request.
+    /// </summary>
     public class TodoRequest
     {
+        /// <summary>
+        /// Gets or sets the identifier of the todo item.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the request is being screamed.
+        /// </summary>
         public bool Scream { get; set; }
     }
 }
--- SportsWebAPI/Responses/TodoResponse.cs
+++ SportsWebAPI/Responses/TodoResponse.cs
@@ -1,14 +1,29 @@
 ﻿namespace SportsWebAPI.Responses
 {
+    /// <summary>
+    /// Class representing the basis for a todo respnose.
+    /// </summary>
     public class TodoResponse
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TodoResponse"/> class.
+        /// </summary>
+        /// <param name="todo">The task to complete.</param>
+        /// <param name="isDone">Flag indicating whether the task is done or not.</param>
         public TodoResponse(string todo, bool isDone)
         {
             Todo = todo;
             IsDone = isDone;
         }
 
+        /// <summary>
+        /// Gets or sets the task to complete.
+        /// </summary>
         public string Todo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag indicating whether the task is complete or not.
+        /// </summary>
         public bool IsDone { get; set; }
     }
 }
